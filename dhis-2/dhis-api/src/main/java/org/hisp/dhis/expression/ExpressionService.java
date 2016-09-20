b42cfdea68e79b5ac8d756c72810481c90643221
Ken Haase <kh@beingmeta.com>
2016-09-20 18:36:33 -0400

    Added pattern for operand sums

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/expression/ExpressionService.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/expression/ExpressionService.java
@@ -82,6 +82,8 @@ public interface ExpressionService
     String CONSTANT_EXPRESSION = "C\\{([a-zA-Z]\\w{10})\\}";
     String OU_GROUP_EXPRESSION = "OUG\\{([a-zA-Z]\\w{10})\\}";
     String DAYS_EXPRESSION = "\\[days\\]";
+    String OPERAND_SUM_EXPRESSION =
+        "(" + OPERAND_EXPRESSION + ")(\\+((" + OPERAND_EXPRESSION + ")" + "|([0-9]+\\.[0-9]+)" + "|([0-9]+)" + "))" + "+";
 
     Pattern VARIABLE_PATTERN = Pattern.compile( VARIABLE_EXPRESSION );
     Pattern OPERAND_PATTERN = Pattern.compile( OPERAND_EXPRESSION );
@@ -92,6 +94,7 @@ public interface ExpressionService
     Pattern CONSTANT_PATTERN = Pattern.compile( CONSTANT_EXPRESSION );
     Pattern OU_GROUP_PATTERN = Pattern.compile( OU_GROUP_EXPRESSION );
     Pattern DAYS_PATTERN = Pattern.compile( DAYS_EXPRESSION );
+    Pattern OPERAND_SUM_PATTERN = Pattern.compile( OPERAND_SUM_EXPRESSION );
 
     /**
      * Adds a new Expression to the database.
